08ae19cbed2067a749849ae791d6271b7323915d
Rodrigo Morales <moralesrodrigo1100@gmail.com>
2023-08-19 12:29:43 -0500

    edit: remove unessential imports and use full module paths in each function call

--- templates.py
+++ templates.py
@@ -1,6 +1,5 @@
 import os
-from os import path
-from aqt import mw as window
+import aqt
 from . import gui, utils
 
 # key names used by Anki
@@ -31,10 +30,10 @@ def import_tmpls():
     if not root: return
     _reload_config()
 
-    notetypes = [item for item in os.listdir(root) if os.path.isdir(path.join(root, item))]
+    notetypes = [item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item))]
 
     global_css = None
-    file = path.join(root, _css_name)
+    file = os.path.join(root, _css_name)
     if os.path.exists(file):
         with open(file, "r", encoding="utf-8") as f:
             global_css = f.read()
@@ -43,11 +42,11 @@ def import_tmpls():
     count_template = 0
     count_no_css = 0
     for name in notetypes:
-        nt = window.col.models.byName(name)
+        nt = aqt.mw.col.models.byName(name)
         if not nt: continue
 
         count = 0
-        file = path.join(root, name, _css_name)
+        file = os.path.join(root, name, _css_name)
         css = None
         if os.path.exists(file):
             with open(file, "r", encoding="utf-8") as f:
@@ -62,8 +61,8 @@ def import_tmpls():
         for tmpl in nt.get(key_name_anki_model_templates, []):
             if key_name_anki_model_template_name not in tmpl:
                 continue
-            file_path_front = path.join(root, name, tmpl[key_name_anki_model_template_name], 'front.html')
-            file_path_back = path.join(root, name, tmpl[key_name_anki_model_template_name], 'back.html')
+            file_path_front = os.path.join(root, name, tmpl[key_name_anki_model_template_name], 'front.html')
+            file_path_back = os.path.join(root, name, tmpl[key_name_anki_model_template_name], 'back.html')
             if os.path.exists(file_path_front):
                 with open(file_path_front, "r", encoding="utf-8") as f:
                     tmpl[key_name_anki_model_template_front] = f.read()
@@ -73,7 +72,7 @@ def import_tmpls():
                     tmpl[key_name_anki_model_template_back] = f.read()
                 count += 1
         try:
-            window.col.models.save(nt)
+            aqt.mw.col.models.save(nt)
         except Exception:
             gui.show_error("note type \"{}\" contains errors!!".format(name))
             continue
@@ -90,7 +89,7 @@ def export_tmpls():
 
     count_notetype = 0
     count_template = 0
-    for nt in window.col.models.all():
+    for nt in aqt.mw.col.models.all():
         try:
             notetype_name = nt.get(key_name_anki_model_template_name)
         except KeyError:
@@ -101,10 +100,10 @@ def export_tmpls():
             gui.show_error("⚠ Leading and/or trailing spaces detected in notetype name \"{}\". They have be removed "
                            "on export. Before reimporting the template, you will need to remove them in the notetype "
                            "name.".format(notetype_name))
-        notetype_path = path.join(root, notetype_name_stripped)
+        notetype_path = os.path.join(root, notetype_name_stripped)
         os.makedirs(notetype_path, exist_ok=True)
         if key_name_anki_model_css in nt:
-            with open(path.join(notetype_path, _css_name), "w", encoding="utf-8") as f:
+            with open(os.path.join(notetype_path, _css_name), "w", encoding="utf-8") as f:
                 f.write(nt[key_name_anki_model_css])
         for tmpl in nt.get(key_name_anki_model_templates, []):
             try:
@@ -113,11 +112,11 @@ def export_tmpls():
                 gui.show_error("A template in notetype \"{}\" has no name!!".format(notetype_name))
                 continue
             # file_path_dir stores the name of the directory for a card type
-            file_path_dir = path.join(notetype_path, tmpl_name) + '/'
+            file_path_dir = os.path.join(notetype_path, tmpl_name) + '/'
             if not os.path.isdir(file_path_dir):
                 os.makedirs(file_path_dir)
-            file_path_front = path.join(file_path_dir, 'front.html')
-            file_path_back = path.join(file_path_dir, 'back.html')
+            file_path_front = os.path.join(file_path_dir, 'front.html')
+            file_path_back = os.path.join(file_path_dir, 'back.html')
             if key_name_anki_model_template_front in tmpl:
                 with open(file_path_front, "w", encoding="utf-8") as f:
                     f.write(tmpl[key_name_anki_model_template_front])
