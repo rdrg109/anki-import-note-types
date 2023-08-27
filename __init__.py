f3463fcf3dc6fe3451a36c54cc2e3ea8b0f18a70
Rodrigo Morales <moralesrodrigo1100@gmail.com>
2023-08-27 13:16:15 -0500

    remove: export_note_types

--- __init__.py
+++ __init__.py
@@ -2,11 +2,10 @@ import aqt
 from . import utilities, config, bind_keys
 
 menu = aqt.QMenu(aqt.mw.form.menuTools)
-menu.setTitle("Import / Export templates")
+menu.setTitle("Import note types")
 aqt.mw.form.menuTools.addAction(menu.menuAction())
 
 actions = [
-    (aqt.qt.QAction("Export to...", menu), utilities.export_note_types),
     (aqt.qt.QAction("Import from...", menu), utilities.import_note_types_from_user_selected_directory),
     (aqt.qt.QAction("Import from default directory", menu), utilities.import_note_types_from_default_directory),
     (aqt.qt.QAction("Edit config", menu), config.edit)
